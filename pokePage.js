47ab564a7e87c217d5035d8a8ef00f8070c45135
BrigittaCifra <brigittacifra@hotmail.com>
2026-02-18 13:04:56 +0100

    feat: added more stats from the api with javascript

--- pokePage.js
+++ pokePage.js
@@ -1,10 +1,19 @@
+//Basic info
 const pokeSprite = document.getElementById("pokeSprite");
 const pokeName = document.getElementById("pokeName");
 const pokeId = document.getElementById("pokeId");
+const pokeDescription = document.getElementById("pokeDescription");
 
+//Stats
+const pokeHP = document.getElementById("pokeHP");
+const pokeAttack = document.getElementById("pokeAttack");
+const pokeDefense = document.getElementById("pokeDefense");
+
+//Type
 const type1 = document.getElementById("type1");
 const type2 = document.getElementById("type2");
 
+//Utility function
 function capitalizeString(string = "") {
     //1.charAt() hämtar första bokstaven
     //2.toUpperCase() konverterar första bokstaven till storbokstav men ändrar inte originella strängen
@@ -28,7 +37,19 @@ fetch(`https://pokeapi.co/api/v2/pokemon/1`)
 
         if (types.length > 1) {
             type2.style.display = "block";
-            type2.textContent = types[1].type.name;
+            type2.textContent = capitalizeString(types[1].type.name);
         }
 
+        //Stats
+        pokeHP.textContent = data.stats[0].base_stat;
+        pokeAttack.textContent = data.stats[1].base_stat;
+        pokeDefense.textContent = data.stats[2].base_stat;
+
+    });
+
+fetch(`https://pokeapi.co/api/v2/characteristic/1`)
+    .then(res => res.json())
+    .then(data => {
+        const descriptions = data.descriptions;
+        pokeDescription.textContent = descriptions[7].description;
     });
